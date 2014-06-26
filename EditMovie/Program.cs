9daaefddad7b08997227c548c8822242a02b8ac0
StevenJV <steven@vorefamily.net>
2014-06-26 10:56:16 -0400

    EditMovie changes data in the RavenDB

---
+++ EditMovie/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Raven.Client;
+using Raven.Client.Document;
+using RavenStuff.Things;
+
+namespace EditMovie
+{
+  class Program
+  {
+    static void Main(string[] args) {
+      using (IDocumentStore documentStore = new DocumentStore() { ConnectionStringName = "MyRavenConStr" }) {
+        documentStore.Initialize();
+        using (IDocumentSession session = documentStore.OpenSession()) {
+          Movie movie = session.Load<Movie>("TestTitle1982");
+          movie.DumpToConsole();
+
+          var newYear = int.Parse(movie.ReleaseYear) + 1;
+          movie.ReleaseYear = newYear.ToString(CultureInfo.InvariantCulture);
+          session.SaveChanges();
+          movie = session.Load<Movie>("TestTitle1982");
+          movie.DumpToConsole();
+          Console.ReadKey();
+
+        }
+      }
+    }
+  }
+}
