0cecd28f6522286e81a9fa7a51faa82e330dae1c
StevenJV <steven@vorefamily.net>
2014-07-01 12:06:44 -0400

    using EditMovie to add actors

--- EditMovie/Program.cs
+++ EditMovie/Program.cs
@@ -16,13 +16,23 @@ namespace EditMovie
       using (IDocumentStore documentStore = new DocumentStore() { ConnectionStringName = "MyRavenConStr" }) {
         documentStore.Initialize();
         using (IDocumentSession session = documentStore.OpenSession()) {
-          Movie movie = session.Load<Movie>("TestTitle1982");
+          Movie movie = session.Load<Movie>("BladeRunner1982");
           movie.DumpToConsole();
-
-          var newYear = int.Parse(movie.ReleaseYear) + 1;
-          movie.ReleaseYear = newYear.ToString(CultureInfo.InvariantCulture);
+          //movie.Director = "Rob Reiner";
+          //add a list of actors to the existing movie
+          movie.ActorList = new Dictionary<string, string>
+          {
+            {"Rutger Hauer", "Roy Batty"},
+            {"Harrison Ford", "Rick Deckard"},
+            {"Sean Young", "Rachel"},
+            {"Edward James Olmos", "Gaff"},
+            {"M. Emmet Walsh", "Bryant"},
+            {"Daryl Hannah","Pris"},
+            {"Morgan Paull","Holden"},
+            {"Joe Turkel","Dr. Eldon Tyell"}
+          };
           session.SaveChanges();
-          movie = session.Load<Movie>("TestTitle1982");
+          movie = session.Load<Movie>("BladeRunner1982");
           movie.DumpToConsole();
           Console.ReadKey();
 
