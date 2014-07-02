ce7eaec623e81e15d016639de9197a00aa1802f3
StevenJV <steven@vorefamily.net>
2014-07-02 12:39:46 -0400

    create/update Actor documents based on Movie documents

---
+++ ActorsFromMovies/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using Raven.Client;
+using Raven.Client.Document;
+using System.Linq;
+using RavenStuff.Things;
+
+namespace ActorsFromMovies
+{
+  internal class Program
+  {
+    private static void Main(string[] args) {
+
+      using (IDocumentStore documentStore = new DocumentStore() { ConnectionStringName = "MyRavenConStr" }) {
+        documentStore.Initialize();
+        using (IDocumentSession movieSession = documentStore.OpenSession()) {
+          var movies = movieSession.Advanced.LuceneQuery<Movie>().ToList();
+          movies.ForEach(movie => {
+                                    using (IDocumentSession actorSession = documentStore.OpenSession())
+                                    {
+                                      Dictionary<string, string> actorList = movie.ActorList;
+                                      foreach (KeyValuePair<string, string> actor in actorList)
+                                      {
+                                        Actor actorInfo = LoadOrCreate(actorSession, actor.Key);
+                                        actorInfo.EnsureMovieExists(movie.Title, movie.ReleaseYear);
+                                      }
+                                      actorSession.SaveChanges();
+                                    }
+          });
+          movieSession.SaveChanges();
+        }
+      }
+    }
+
+    private static Actor LoadOrCreate(IDocumentSession session, string actorName) {
+      var actor = session.Load<Actor>(ActorId(actorName));
+      if (actor != null) return actor;
+      var pActor = new Actor { Name = actorName };
+      session.Store(pActor);
+      session.SaveChanges();
+      return pActor;
+    }
+
+    private static string ActorId(string actorName) {
+      const int maxLength = 1023;
+      var id = actorName.Replace(" ", string.Empty);
+      id = id.Replace("\\", string.Empty);
+      id = id.Replace("'", string.Empty);
+      return id.Length <= maxLength ? id : id.Substring(0, maxLength);
+    }
+  }
+}
