d0cc36881392685d3e1f4bba500a1bef952ad05c
StevenJV <steven@vorefamily.net>
2014-07-02 14:15:05 -0400

    added initial controllers/views for Actor

--- Actor/Actor.cs
+++ Actor/Actor.cs
@@ -4,26 +4,31 @@ using System.ComponentModel;
 // ReSharper disable once CheckNamespace
 namespace RavenStuff.Things
 {
-    public class Actor
-    {
-      private string _id;
-      public string Id {
-        get {
-          const int maxLength = 1023;
-          _id = this.Name.Replace(" ", string.Empty);
-          _id = _id.Replace("\\", string.Empty);
-          _id = _id.Replace("'", string.Empty);
-          return _id.Length <= maxLength ? _id : _id.Substring(0, maxLength);
-        }
-        set { this._id = value; }
+  public class Actor
+  {
+    private string _id;
+    public string Id {
+      get {
+        const int maxLength = 1023;
+        _id = this.Name.Replace(" ", string.Empty);
+        _id = _id.Replace("\\", string.Empty);
+        _id = _id.Replace("'", string.Empty);
+        return _id.Length <= maxLength ? _id : _id.Substring(0, maxLength);
       }
-      public string Name { get; set; }
-      public Dictionary<string, string> MovieList { get; set; } // title, year 
-      public DateTime BirthDate { get; set; }
-
-      public void EnsureMovieExists(string movieName, string movieYear) {
-        if (MovieList == null) MovieList = new Dictionary<string, string>();
-        if (!MovieList.ContainsKey(movieName)) MovieList.Add(movieName, movieYear);
-      }
+      set { this._id = value; }
     }
+    public string Name { get; set; }
+    public Dictionary<string, string> MovieList { get; set; } // title, year 
+    public DateTime BirthDate { get; set; }
+
+    public void EnsureMovieExists(string movieName, string movieYear) {
+      if (MovieList == null) MovieList = new Dictionary<string, string>();
+      if (!MovieList.ContainsKey(movieName)) MovieList.Add(movieName, movieYear);
+    }
+
+    public string HtmlRow() {
+      string output = "<a href=\"/Actor/Details/" + Id + "\">" + Name + "</a>";
+      return output;
+    }
+  }
 }
---
+++ RMDB/Controllers/actorController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Linq;
+using Raven.Client;
+using Raven.Client.Document;
+using RavenStuff.Things;
+
+namespace RMDB.Controllers
+{
+  public class ActorController : Controller
+  {
+    //
+    // GET: /actor/ - return a list of actors
+    public ActionResult Index()
+        {
+          using (IDocumentStore documentStore = new DocumentStore() {ConnectionStringName = "MyRavenConStr"})
+          {
+            documentStore.Initialize();
+            using (IDocumentSession session = documentStore.OpenSession())
+            {
+              var actorList = new List<string>();
+              var actors = session.Advanced.LuceneQuery<Actor>().ToList();
+              actors.ForEach(actor => actorList.Add(actor.HtmlRow()));
+              ViewBag.data = actorList;
+              return View();
+            }
+          }
+        }
+
+    // GET: /actor/details/CarrieFisher - returns details of a specific actor
+    public ActionResult Details(string id)
+    {
+      using (IDocumentStore documentStore = new DocumentStore() {ConnectionStringName = "MyRavenConStr"})
+      {
+        documentStore.Initialize();
+        using (IDocumentSession session = documentStore.OpenSession())
+        {
+          var actorInfo = session.Load<Actor>(id);
+          ViewBag.data = actorInfo;
+          return View();
+        }
+      } 
+    }
+
+  }
+}
