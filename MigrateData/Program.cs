8b60cc18dea06bd4067b16e6d40605a28e13b94e
StevenJV <steven@vorefamily.net>
2014-07-02 12:42:20 -0400

    removal of unused method

--- MigrateData/Program.cs
+++ MigrateData/Program.cs
@@ -30,13 +30,5 @@ namespace MigrateData
       };
       session.Store(movieInfo);
     }
-
-    //TODO: move this to the Movie class? 
-    private static string CreateMovieId(string title, string year) {
-      const int maxLength = 1023;
-      var ID = title.Replace(" ", string.Empty) + year;
-      ID = ID.Replace("\\", string.Empty);
-      return ID.Length <= maxLength ? ID : ID.Substring(0, maxLength);
-    }
   }
 }
