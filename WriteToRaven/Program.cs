7ffc0bb76f5bfcbb57f490b4fedc72ff7f815dba
StevenJV <steven@vorefamily.net>
2014-07-02 13:14:16 -0400

    add ability to create Actor document directly

--- WriteToRaven/Program.cs
+++ WriteToRaven/Program.cs
@@ -11,33 +11,51 @@ namespace WriteToRaven
   {
     static void Main(string[] args) {
       using (IDocumentStore documentStore = new DocumentStore() { ConnectionStringName = "MyRavenConStr" }) {
-        var movieInfo = new Movie
-        {
-          Title = "Ender's Game",
-          Quote = "The enemy's gate is down.",
-          Director = "Gavin Hood",
-          CreatedDate = DateTime.UtcNow.ToString(CultureInfo.InvariantCulture),
-          ReleaseYear = "2013",
-          ActorList = new Dictionary<string, string>
-          {
-            {"Harrison Ford", "Colnel Graff"},
-            {"Asa Butterfield", "Ender Wiggin"},
-            {"Hailee Steinfield", "Petra Arkanian"},
-            {"Abigail Breslin", "Valentine Wiggin"},
-            {"Ben Kingsley", "Mazer Rackham"}
-          }
-        };
-        documentStore.Initialize();
-        using (IDocumentSession session = documentStore.OpenSession())
-        {
-          session.Store(movieInfo);
-          session.SaveChanges();
-        }
-        
+        //CreateMovieDocument(documentStore);
+        //CreateActorDocument(documentStore);
       }
 
     }
 
+    private static void CreateActorDocument(IDocumentStore documentStore)
+    {
+      var actorInfo = new Actor
+      {
+        Name = "Angelina Jolie",
+        BirthDate = new DateTime(1975, 6, 4)
+      };
+      documentStore.Initialize();
+      using (IDocumentSession session = documentStore.OpenSession())
+      {
+        session.Store(actorInfo);
+        session.SaveChanges();
+      }
+    }
 
+    private static void CreateMovieDocument(IDocumentStore documentStore)
+    {
+      var movieInfo = new Movie
+      {
+        Title = "Ender's Game",
+        Quote = "The enemy's gate is down.",
+        Director = "Gavin Hood",
+        CreatedDate = DateTime.UtcNow.ToString(CultureInfo.InvariantCulture),
+        ReleaseYear = "2013",
+        ActorList = new Dictionary<string, string>
+        {
+          {"Harrison Ford", "Colnel Graff"},
+          {"Asa Butterfield", "Ender Wiggin"},
+          {"Hailee Steinfield", "Petra Arkanian"},
+          {"Abigail Breslin", "Valentine Wiggin"},
+          {"Ben Kingsley", "Mazer Rackham"}
+        }
+      };
+      documentStore.Initialize();
+      using (IDocumentSession session = documentStore.OpenSession())
+      {
+        session.Store(movieInfo);
+        session.SaveChanges();
+      }
+    }
   }
 }
